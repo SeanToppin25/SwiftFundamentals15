2ff85e05bf2e20a9f9ee94a128927ad740638734
SeanToppin25 <rockysean25@gmail.com>
2025-10-24 16:43:08 -0400

    Update Contents.swift

--- Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
+++ Unit1/Lesson1_2/Lab_Introduction.playground/Pages/1. Exercise - Use Playgrounds.xcplaygroundpage/Contents.swift
@@ -10,12 +10,15 @@ print("How to use playgrounds to make writing Swift fun and simple")
 /*:
  Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the `print` function to display the song title and artist.
  */
-
+print("Happy birthday to you, by Preston Ware Orem")
 
 /*:
  Use multiple `print` functions to write out some of the lyrics to the song.
  */
-
+print("Happy birthday to you!")
+print("Happy birthday to you!")
+print("Happy birthday dear insert name!")
+print("Happy birthday to you!")
 
 
 /*:
