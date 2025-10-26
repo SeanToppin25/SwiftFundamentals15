ba63c8b367c0f290b8e507032ed8fa81d1f299fa
SeanToppin25 <rockysean25@gmail.com>
2025-10-26 16:23:05 -0400

    Update Contents.swift

--- Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
+++ Unit1/Lesson1_3/Lab_Constants_Variables.playground/Contents.swift
@@ -3,26 +3,30 @@
  
  Declare a constant called `friends` to represent the number of friends you have on social media. Give it a value between 50 and 1000. Print out the value by referencing your constant.
  */
-
-
+let friends = 347
+print("Number of friends: \(friends)")
 /*:
  Now assume you go through and remove a lot of your friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is between 1 and 900.
  */
-
-
+//deleted the text
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
-
+print("You cant change constant values, and so it doesnt compile if you try to.")
 
 /*:
  Declare a variable `age` and set it to your own age. Print `age` to the console.
  */
 
+var age = 21
+print("Current age: \(age)")
 
 /*:
  Now pretend you just had a birthday, and update the `age` variable accordingly. Print `age` to the console.
  */
+
+age += 1
+print("New age is: \(age)")
 
 /*:
  
@@ -32,19 +36,34 @@
 /*:
  Create a double variable with a value of 1.1. Update it to 2.2, 3.3, and 4.4, printing out the value after each assignment (again by referencing the variable you created).
  */
-
+var decimalValue = 1.1
+print("\(decimalValue)")
+decimalValue = 2.2
+print("\(decimalValue)")
+decimalValue = 3.3
+print("\(decimalValue)")
+decimalValue = 4.4
+print("\(decimalValue)")
 
 /*:
  Create a boolean variable and set it to `true`. Print the variable, then assign it a value of `false`, and print it again.
  */
-
+var isOnline = true
+print("\(isOnline)")
+isOnline = false
+print("\(isOnline)")
 
 /*:
  Create two variables, one with a value of 0, the other with a value of 0.0. Try to assign the second variable to the first, and you will receive an error. Add the necessary type annotation that will allow the second variable to be assigned to the first.
  */
-
+var intZero: Int = 0
+var doubleZero: Double = 0.0
+intZero = Int(doubleZero) 
+print("\(intZero)")
 
 /*:
  Create a variable integer with a value of 1,000,000,000, ensuring that you format it so it is more readable (i.e. it's hard to read 1000000000, so make it easier to read).
  */
+let bigNumber = 1_000_000_000
+print("\(bigNumber)")
 
