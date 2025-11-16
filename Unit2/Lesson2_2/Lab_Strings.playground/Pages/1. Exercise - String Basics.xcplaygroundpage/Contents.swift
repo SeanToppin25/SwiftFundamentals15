d5d165d5f1e7037dd1f615e72d78a006ea029be5
SeanToppin25 <rockysean25@gmail.com>
2025-11-15 22:54:57 -0500

    Update Contents.swift

--- Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_2/Lab_Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@
  
  Create a `name` constant and assign it a string literal representing your name.
  */
-
+let name = "Sean Toppin"
 
 /*:
  Create a `favoriteQuote` constant and assign it the following string literal:
@@ -17,8 +17,18 @@
  */
 
 
+let favoriteQuote = "My favorite quote is \"is mayonnaise an instrument.\""
+print(favoriteQuote)
+
+
 //:  Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
 let emptyString = ""
+
+if emptyString.isEmpty {
+    print("There's nothing here")
+} else {
+    print("It's not as empty as I thought")
+}
 
 
 /*:
