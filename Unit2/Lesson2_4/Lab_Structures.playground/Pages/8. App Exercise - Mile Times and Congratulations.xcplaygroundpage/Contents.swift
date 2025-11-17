b3116aabcd4fa549e094444baf006eaab5f56d34
SeanToppin25 <rockysean25@gmail.com>
2025-11-16 19:02:58 -0500

    Update Contents.swift

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
@@ -7,21 +7,34 @@
  
  Create an instance of `RunningWorkout` and print the `averageMileTime` property. Check that it works properly.
  */
+
 struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
     
+    var averageMileTime: Double {
+        return time / (distance / 1600)
+    }
 }
 
+let run = RunningWorkout(distance: 3200, time: 30.6, elevation: 100)
+print(run.averageMileTime)
 
 /*:
  In other app exercises, you've provided encouraging messages to the user based on how many steps they've completed. A great place to check whether or not you should display something to the user is in a property observer.
  
  In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
  */
+
 struct Steps {
-    var steps: Int
+    var steps: Int {
+        willSet {
+            if newValue == goal {
+                print("Congratulations! You've reached your goal of \(goal) steps!")
+            }
+        }
+    }
     var goal: Int
     
     mutating func takeStep() {
@@ -29,6 +42,10 @@ struct Steps {
     }
 }
 
+var today = Steps(steps: 9999, goal: 10000)
+print("Steps before: \(today.steps)")
+today.takeStep()
+print("Steps after: \(today.steps)")
 
 /*:
 [Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Properties and Methods](@next)
